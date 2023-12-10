817b7ee4c8c38ba0e3192933ba78184a795ed5ef
suryajacks <suryajacks16@gmail.com>
2023-12-10 19:43:28 +0530

    #####Subnet us-east-1c

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "surya_instance" {
   ami           = "ami-0230bd60aa48260c6" # Amazon Linux 2 AMI, replace with your desired AMI
   instance_type = "t2.micro"               # Specify your desired instance type
+  subnet_id     = "subnet-0fdc25c8ba32b15ad" # 1c-id -N.virginia 
 
   tags = {
     Name = "example-instance"
