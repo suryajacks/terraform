d4ec944dd8ee6869932af079761885f19516fc42
suryajacks <suryajacks16@gmail.com>
2023-12-10 18:27:47 +0530

    aws_ec2_instance

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1" # Specify your desired AWS region
+}
+
+resource "aws_instance" "surya_instance" {
+  ami           = "ami-0230bd60aa48260c6" # Amazon Linux 2 AMI, replace with your desired AMI
+  instance_type = "t2.micro"               # Specify your desired instance type
+
+  tags = {
+    Name = "example-instance"
+  }
+}
+
+#ec2 instance
